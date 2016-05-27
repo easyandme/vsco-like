6aa3899f4530d7b58d05bc88b113d151a54e53cb
Maxim Xu <xujialv@gmail.com>
2016-05-27 17:55:30 +0800

    added github button; minified js

--- js/function.js
+++ js/function.js
@@ -56,7 +56,7 @@
     document.getElementById('file').onchange=function(){    
            var filereader = new FileReader();
            filereader.onload = function(e) { 
-                 document.getElementById('convert').disabled = false;
+                document.getElementById('convert').disabled = false;
                 document.getElementById('convert').innerHTML = '<a>Convert</a>';
                 document.getElementById('dlbtn').disabled = true;
                img.onload = function() {    
@@ -111,11 +111,119 @@
             document.getElementById('convert').disabled = true;
     }
 
+    function kodak() {
+            document.getElementById('convert').innerHTML = 'Rendering...';
+            Caman("#myOutput", function () { 
+              refresh(); 
+              this.saturation(-10);
+              this.noise(5); 
+              this.curves('rgb', [0, 47], [43, 54], [79, 75], [134, 133], [189, 188], [255, 245]); 
+              this.curves('r', [0, 0], [33, 13], [82, 64], [173, 202], [255, 255]);
+              this.curves('g', [0, 0], [33, 31], [83, 74], [113, 120], [177, 200], [255, 255]);
+              this.curves('b', [0, 0], [80, 62], [114, 121], [177, 199], [255, 255]); 
+              this.render(
+                    function() {
+                    document.getElementById('dlbtn').disabled = false;
+                    document.getElementById('convert').innerHTML = 'Rendered';
+                }); 
+            }); 
+            document.getElementById('convert').disabled = true;
+    }
+
+    function royal() { 
+            document.getElementById('convert').innerHTML = 'Rendering...';
+            Caman("#myOutput", function () { 
+              refresh();  
+              this.noise(4); 
+              this.curves('rgb', [0, 12], [77, 74], [131, 133], [184, 178], [227, 205], [253, 231]); 
+              this.curves('r', [0, 0], [35, 14], [66, 46], [121, 124], [184, 198], [255, 255]);
+              this.curves('g', [0, 0], [44, 25], [120, 124], [183, 199], [217, 226], [255, 255]);
+              this.curves('b', [0, 0], [32, 18], [76, 61], [118, 125], [220, 223], [255, 248]); 
+              this.render(
+                    function() {
+                    document.getElementById('dlbtn').disabled = false;
+                    document.getElementById('convert').innerHTML = 'Rendered';
+                }); 
+            }); 
+            document.getElementById('convert').disabled = true;
+    }
+
+    function vistawarm() { 
+            document.getElementById('convert').innerHTML = 'Rendering...';
+            Caman("#myOutput", function () { 
+              refresh();  
+              this.noise(5); 
+              this.saturation(-5);
+              this.channels({
+                  red: 8,
+                  green: 0,
+                  blue: -8,
+              })
+              this.curves('rgb', [0, 48], [77, 76], [133, 133], [189, 187], [255, 245]); 
+              this.curves('r', [0, 0], [30, 16], [75, 64], [113, 122], [174, 197], [255, 255]);
+              this.curves('g', [0, 0], [32, 11], [114, 120], [178, 200], [255, 255]);
+              this.curves('b', [0, 0], [80, 49], [121, 119], [174, 194], [255, 255]); 
+              this.render(
+                    function() {
+                    document.getElementById('dlbtn').disabled = false;
+                    document.getElementById('convert').innerHTML = 'Rendered';
+                }); 
+            }); 
+            document.getElementById('convert').disabled = true;
+    }
+
+    function polaroid() { 
+            document.getElementById('convert').innerHTML = 'Rendering...';
+            Caman("#myOutput", function () { 
+              refresh();  
+              this.saturation(-10);
+              this.channels({
+                  red: -5,
+                  green: -8,
+                  blue: -10,
+              })
+              this.curves('rgb', [0, 54], [48, 74], [161, 165], [255, 248]); 
+              this.curves('r', [0, 29], [36, 44], [118, 118], [176, 187], [227, 234], [255, 255]);
+              this.curves('g', [0, 27], [29, 39], [123, 116], [166, 174], [214, 229], [255, 255]);
+              this.curves('b', [0, 0], [49, 27], [115, 118], [181, 201], [255, 232]); 
+              this.render(
+                    function() {
+                    document.getElementById('dlbtn').disabled = false;
+                    document.getElementById('convert').innerHTML = 'Rendered';
+                }); 
+            }); 
+            document.getElementById('convert').disabled = true;
+    }
+
+    function fujih() { 
+            document.getElementById('convert').innerHTML = 'Rendering...';
+            Caman("#myOutput", function () { 
+              refresh();  
+              this.saturation(5);
+              this.channels({
+                  red: 5,
+                  green: 4,
+                  blue: -7,
+              })
+              this.noise(3);
+              this.curves('rgb', [0, 21], [65, 72], [173, 174], [255, 255]); 
+              this.curves('r', [1, 4], [46, 39], [115, 127], [188, 199], [255, 255]);
+              this.curves('g', [0, 6], [44, 36], [116, 129], [144, 158], [188, 196], [255, 252]);
+              this.curves('b', [0, 0], [50, 36], [118, 129], [190, 198], [255, 250]); 
+              this.render(
+                    function() {
+                    document.getElementById('dlbtn').disabled = false;
+                    document.getElementById('convert').innerHTML = 'Rendered';
+                }); 
+            }); 
+            document.getElementById('convert').disabled = true;
+    }
+
     function vista() {
             document.getElementById('convert').innerHTML = 'Rendering...';
             Caman("#myOutput", function () { 
               refresh();
-              this.noise(3); 
+              this.noise(2); 
               this.curves('r', [0, 0], [42, 9], [78, 45], [126, 122], [182, 204], [255, 255]);
               this.curves('g', [0, 0], [36, 10], [76, 46], [124, 122], [181, 201], [219, 230], [255, 255]);
               this.curves('b', [0, 0], [36, 10], [76, 46], [122, 118], [183, 206], [222, 235], [255, 255]); 
